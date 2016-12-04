adcb38893c2992313b159fd43dbf23dbd535d904
egevirgen <egevirgen@gmail.com>
2016-12-04 21:09:02 +0300

    Harita Activity BugFix ve optimizasyonlar

--- app/src/main/java/com/corp/theapp/MapsActivity.java
+++ app/src/main/java/com/corp/theapp/MapsActivity.java
@@ -20,7 +20,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -43,7 +42,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -69,6 +67,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     RelativeLayout relativeLayout;
     ImageButton locationbutton;
     ArrayList<PlaceNames> arraylist = new ArrayList<>();
+    EditText searchEditText;
 
     public void Initialize(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -82,8 +81,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         locationbutton=(ImageButton)findViewById(R.id.location);
         relativeLayout = (RelativeLayout)findViewById(R.id.deneme);
         editsearch = (SearchView) findViewById(R.id.search);
-        final EditText searchEditText = (EditText) editsearch.findViewById(editsearch.getContext().getResources().getIdentifier("android:id/search_src_text", null, null));
-
+        searchEditText = (EditText) editsearch.findViewById(editsearch.getContext().getResources().getIdentifier("android:id/search_src_text", null, null));
         searchEditText.setTextColor(Color.parseColor("#212121"));
         searchEditText.setHintTextColor(Color.parseColor("#616161"));
         searchEditText.setTextSize(14f);
@@ -103,13 +101,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
         mBottomSheetBehavior.setPeekHeight(0);
         mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-        bottomSheet.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                Log.e("KEY PRESS = ","View ="+view+"| i ="+i+"| keyevent="+keyEvent);
-                return false;
-            }
-        });
+
         // internetten array çekicez TODO
         placeList = new String[]{"Yemen Kahvesi--Antalya--Altınkum--36.864727f--30.634416f", "Osmanlı Kahvecisi--Antalya--Bayındır--36.901504--30.672853", "Burger King--Antalya--Meltem--36.892676--30.667396", "Starbucks--Antalya--Konyaaltı--36.884583--30.659156","Rıhtım Döner--Antalya--Akdeniz Üniversitesi--36.898750--30.653261"};
 
@@ -130,9 +122,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+
         searchImgId = getResources().getIdentifier("android:id/search_mag_icon", null, null);
         ivIcon = (ImageView) editsearch.findViewById(searchImgId);
-
 
         //searchview button listener
         editsearch.setOnClickListener(new View.OnClickListener() {
@@ -162,7 +154,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if (newState == BottomSheetBehavior.STATE_EXPANDED) {
 
                     ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), Color.parseColor("#00FFFFFF"), Color.parseColor("#E0E0E0"));
-                    colorAnimation.setDuration(250); // milliseconds
+                    colorAnimation.setDuration(100); // milliseconds
                     colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
                         @Override
@@ -174,7 +166,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     colorAnimation.start();
 
                     if(ivIcon!=null) {
-                        ivIcon.setImageResource(R.drawable.search_button);
+                        ivIcon.setImageResource(R.drawable.search_back_button);
                     }
 
                     editsearch.requestFocus();
@@ -184,7 +176,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 else if(newState==BottomSheetBehavior.STATE_COLLAPSED){
 
                     ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), Color.parseColor("#E0E0E0"), Color.parseColor("#00FFFFFF"));
-                    colorAnimation.setDuration(250); // milliseconds
+                    colorAnimation.setDuration(200); // milliseconds
                     colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
                         @Override
@@ -210,7 +202,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         locationbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Location();
+                if(mBottomSheetBehavior.getState()==BottomSheetBehavior.STATE_EXPANDED)
+                    mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                Location("Animate");
             }
         });
 
@@ -267,9 +261,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        final SearchView searchView = (SearchView) findViewById(R.id.search);
-        final EditText searchEditText = (EditText) searchView.findViewById(searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null));
-
         switch (requestCode) {
             case RESULT_SPEECH: {
                 if (resultCode == RESULT_OK && null != data) {
@@ -310,13 +301,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         Initialize();
         mMap = googleMap;
-        MapStyleOptions mapStyleOptions = MapStyleOptions.loadRawResourceStyle(this,R.raw.style_json);
-        mMap.setMapStyle(mapStyleOptions);
         adapter = new ListViewAdapter(this, arraylist, mMap, mBottomSheetBehavior);
         list.setAdapter(adapter);
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Location();
+            Location("Move");
             mMap.setMyLocationEnabled(true);
         }
 
@@ -329,7 +318,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    public void Location() {
+    public void Location(final String type) {
         Handler mainHandler = new Handler(getApplicationContext().getMainLooper());
 
         Runnable myRunnable = new Runnable() {
@@ -337,8 +326,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void run() {try {
                 latlong = mMap.getMyLocation();
                 latlong1 = new LatLng(latlong.getLatitude(), latlong.getLongitude());
-         // Animate fonksiyonu map açıldığında dönecek şekilde ayarlanacak. TODO //
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlong1,12));
+
+                if(type.equals("Move"))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlong1,12));
+                else if(type.equals("Animate"))
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlong1,12));
             } catch (NullPointerException | IllegalStateException e) {
 
             }}
